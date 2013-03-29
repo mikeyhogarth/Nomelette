c29b6ba365109eccb6ef80d8ef6acd900e230cbd
= <mikehogarth20@hotmail.com>
2013-03-29 13:59:17 +0000

    Initial Commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+module ApplicationHelper
+
+	def page_title(title)
+		content_for :title do
+  			title
+  		end
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomelette::Application.config.secret_token = 'c16b463f0b710acafb7c64f972ee2f01154145ff8e4d3e8a2dc8658948a9671b88e9054983721f07298df47b7a22406bafbcdab8f21de09cba982755654b27ea'
