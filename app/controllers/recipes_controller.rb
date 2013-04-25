2d7740ca302686b3136fcaf5d7f8471a0ec4ea8b
Mikey Hogarth <mikehogarth20@hotmail.com>
2013-04-25 18:22:15 +0100

    yaml update, tags

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,9 +10,20 @@ class RecipesController < ApplicationController
 
 	def tagged_with
 		redirect_to root_path and return unless params[:tag]
+		@tag = params[:tag].gsub("-"," ")
 
-		@tag = params[:tag].camelize
-		@recipes = Recipe.tagged_with(@tag)
+		tags_to_match = [@tag]
+
+		#add the alternative, either the plural or the singular, to the search
+		
+		if @tag.ends_with? "s"
+			tags_to_match << @tag[0...@tag.length-1] 
+		else
+			tags_to_match << @tag.pluralize 
+		end
+
+		@recipes = Recipe.tagged_with(tags_to_match, :any => true)
+		
 		render "index"
 	end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ require "yaml"
 auth_keys = YAML.load_file("#{Rails.root}/config/private_config/oauth_keys.yml")
 
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :facebook, auth_keys["facebook_key"], auth_keys["facebook_secret"]
+  provider :facebook, auth_keys["facebook"]["key"], auth_keys["facebook"]["secret"]
   #provider :twitter, auth_keys["twitter_consumer_key"], auth_keys["twitter_consumer_secret"]
 end
 
