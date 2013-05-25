5587e439062145d34bf34a9d6890545f86aeca3e
= <mikehogarth20@hotmail.com>
2013-05-25 19:52:37 +0100

    Adjusting number of images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index  	
-  	@latest_recipes = Recipe.latest 5
+  	@latest_recipes = Recipe.latest 4
   	@featured_recipes = Recipe.featured 6
   	@category_types = CategoryType.includes(:categories).joins([:categories])
-  	@recipes_with_images = Recipe.with_image.limit(6)
+  	@recipes_with_images = Recipe.with_image.limit(4)
   end
 
   def about
