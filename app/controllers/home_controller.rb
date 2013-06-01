49c35adb8e8ddadb2094d660cd3ac580aeffe2e1
= <mikehogarth20@hotmail.com>
2013-06-01 13:27:30 +0100

    More front end changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index  	
-  	@latest_recipes = Recipe.latest 4
+  	@latest_recipes = Recipe.latest 10
   	@featured_recipes = Recipe.featured 6
   	@category_types = CategoryType.includes(:categories).joins([:categories])
   	@recipes_with_images = Recipe.with_image.limit(4)
