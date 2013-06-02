8806490ae9b729ff47491b689eae1c683545e449
= <mikehogarth20@hotmail.com>
2013-06-02 11:54:55 +0100

    Tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
   
   def index  	
   	@latest_recipes = Recipe.latest 10
+
   	@featured_recipes = Recipe.featured 6
+
     @recipes_with_images = Recipe.with_image.limit 2
 
   	@category_types = CategoryType.includes(:categories).joins([:categories])
