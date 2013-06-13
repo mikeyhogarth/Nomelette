052257bbcf7d2a02589b3d8747a1d292b97b839c
= <mikehogarth20@hotmail.com>
2013-06-13 21:39:51 +0100

    Tidying up home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index  	
-  	@latest_recipes = Recipe.latest 15
+  	@latest_recipes = Recipe.latest 20
   	@featured_recipes = Recipe.featured 6
     @popular_recipes = Recipe.with_description.with_image.shuffle.take(4)
     @recipe_of_the_day = Recipe.of_the_day
