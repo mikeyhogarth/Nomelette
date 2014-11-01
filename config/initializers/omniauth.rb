d719a7ba5f1640163e4b318784f62483d47ba3df
Mikey Hogarth <mikehogarth20@hotmail.com>
2014-11-01 10:25:09 +0000

    Upgrade to rails 3.2.18

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,11 @@
 require "yaml"
 
-auth_keys = YAML.load_file("#{Rails.root}/config/private_config/oauth_keys.yml")
+auth_keys = YAML.load_file("#{Rails.root}/config/private_config/oauth_keys.yml") rescue {} 
 
-Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :facebook, auth_keys["facebook"]["key"], auth_keys["facebook"]["secret"]
-  #provider :twitter, auth_keys["twitter_consumer_key"], auth_keys["twitter_consumer_secret"]
+if auth_keys.any?
+  Rails.application.config.middleware.use OmniAuth::Builder do  
+    provider :facebook, auth_keys["facebook"]["key"], auth_keys["facebook"]["secret"]
+    #provider :twitter, auth_keys["twitter_consumer_key"], auth_keys["twitter_consumer_secret"]
+  end
 end
 
